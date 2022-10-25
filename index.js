78475f10ca3a7e9f2aadd892ca6076ea329198d6
Aaron Magno <aefmagno@gmaik.com>
2022-10-25 19:54:11 +0800

    stdin implementation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { argv } = require('node:process')
+const { argv, stdin } = require('node:process')
 const fs = require('fs')
 
 if (argv.length === 4 || argv.length === 5) {
@@ -33,6 +33,17 @@ if (argv.length === 4 || argv.length === 5) {
   } catch (err) {
     console.log('The file does not exist.')
   }
+} else if (argv.length === 2) {
+  stdin.on('readable', () => {
+    let data = ''
+    while ((data = stdin.read()) !== null) {
+      const stringData = data.toString().split('\n')
+      for (let i = 0, ctr = 1; i < stringData.length; i += 1, ctr += 1) {
+        console.log(`${ctr} ${stringData[i]}`)
+      }
+    }
+  })
 } else {
-  console.log('Please Input Three Arguments At Most')
+  console.log(argv.length)
+  console.log('Invalid Argument Count.')
 }
